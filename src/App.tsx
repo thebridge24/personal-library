3572ed5e6cdc9d7b009f886e0ff42703bb5894a4
Alexander Bridge <alexanderchrist203@gmail.com>
2026-01-29 13:29:12 +0100

    made final changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import alexanderLogo from "./assets/alexander_logo.png";
+import alexanderLogo from "../public/alexander_logo.png";
 
 /* ======================
    Data
@@ -93,13 +93,13 @@ function App() {
               }}
               aria-label="keypad"
               className={`w-[5rem] h-[5rem] rounded-full ${isError ? "animate-shake" : unlocked ? "animate-slide-up" : ""} 
-               text-2xl font-medium bg-black/5 active:rounded-xl active:scale-95 transition duration-200 hover:bg-black/80 hover:text-white`}
+               text-2xl font-medium bg-black/5 active:rounded-xl active:scale-95 transition duration-200 active:bg-black/80 active:text-white`}
             >
               {key}
             </button>
           ))}
 
-         {pin.length > 0 && <button onClick={clearKey} aria-label="clear button" className="animate-fade absolute left-4 bottom-2 p-4 rounded-full duration-100 ">
+         {pin.length > 0 && <button onClick={clearKey} aria-label="clear button" className={`animate-fade ${unlocked ? "animate-slide-up" : ""} absolute left-4 bottom-2 p-4 rounded-full duration-100 `}>
             <svg className="size-8" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="m 7 2 c -0.832031 0 -1.558594 0.34375 -2.292969 0.78125 s -1.464843 1.003906 -2.128906 1.597656 c -0.660156 0.597656 -1.253906 1.222656 -1.707031 1.796875 c -0.226563 0.289063 -0.417969 0.5625 -0.570313 0.835938 c -0.152343 0.277343 -0.300781 0.53125 -0.300781 0.988281 s 0.148438 0.710938 0.300781 0.984375 c 0.152344 0.277344 0.34375 0.550781 0.570313 0.835937 c 0.453125 0.578126 1.046875 1.203126 1.707031 1.796876 c 0.664063 0.597656 1.394531 1.164062 2.128906 1.601562 s 1.460938 0.78125 2.292969 0.78125 h 6 c 1.644531 0 3 -1.355469 3 -3 v -6 c 0 -1.644531 -1.355469 -3 -3 -3 z m 1 3 c 0.265625 0 0.519531 0.105469 0.707031 0.292969 l 1.292969 1.292969 l 1.292969 -1.292969 c 0.1875 -0.1875 0.441406 -0.292969 0.707031 -0.292969 s 0.519531 0.105469 0.707031 0.292969 c 0.390625 0.390625 0.390625 1.023437 0 1.414062 l -1.292969 1.292969 l 1.292969 1.292969 c 0.390625 0.390625 0.390625 1.023437 0 1.414062 s -1.023437 0.390625 -1.414062 0 l -1.292969 -1.292969 l -1.292969 1.292969 c -0.390625 0.390625 -1.023437 0.390625 -1.414062 0 s -0.390625 -1.023437 0 -1.414062 l 1.292969 -1.292969 l -1.292969 -1.292969 c -0.390625 -0.390625 -0.390625 -1.023437 0 -1.414062 c 0.1875 -0.1875 0.441406 -0.292969 0.707031 -0.292969 z m 0 0" fill="#000000"></path> </g></svg>
           </button>}
 
