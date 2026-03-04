e4d8236d0b9e7bab145549a9bdd75b1b32ecb37b
Alexander Bridge <alexanderchrist203@gmail.com>
2026-03-04 05:58:49 +0100

    final version for now

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Sun, Moon } from "lucide-react";
    Data
 ====================== */
 const CORRECT_PIN = "1234";
-const PROJECT_LINK = "https://your-project-link.com";
+const PROJECT_LINK = "https://wa.me/2349160979848?text=Hi%20Alexander%2C%20I%27d%20like%20to%20see%20your%20projects.";
 const KEYS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 /* ======================
@@ -16,7 +16,7 @@ function App() {
   const [pin, setPin] = useState("");
   const [showPasswordUI, setShowPasswordUI] = useState(false);
   const [isError, setIsError] = useState(false);
-  const [theme, setTheme] = useState<"light" | "dark">("light");
+  const [theme, setTheme] = useState<"dark" | "light">("dark");
 
   /* ======================
      Effects
@@ -55,15 +55,13 @@ function App() {
   };
 
   const toggleTheme = () => {
-    setTheme((prev) => (prev === "dark" ? "light" : "dark"));
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
   };
 
   return (
-    <div className="relative w-full h-screen overflow-hidden bg-white dark:bg-black transition-colors duration-700">
+    <div className="relative w-full min-h-screen overflow-hidden bg-white dark:bg-black transition-colors duration-700">
       {/* Neon Animated Background */}
-      <div className="absolute inset-0 z-0">
-        <div className="absolute w-[600px] h-[600px] bg-black dark:bg-white blur-[180px] opacity-[0.09] animate-neon-slow left-1/3 top-1/3 rounded-full" />
-      </div>
+      <div className="container absolute scale-[1.4]"></div>
 
       {/* Theme Toggle */}
       {/* Theme Toggle */}
@@ -71,7 +69,7 @@ function App() {
         onClick={toggleTheme}
         aria-label="Toggle Theme"
         className="absolute top-6 right-6 
-  p-3 rounded-full 
+  p-3 rounded-full z-[999] hidden
   border border-black/30 dark:border-white/30
   text-black dark:text-white
   hover:scale-110 active:scale-95
@@ -87,7 +85,7 @@ function App() {
       {/* ======================
           Home Screen
       ====================== */}
-      <div className="w-full h-screen flex flex-col items-center justify-center gap-6 text-black dark:text-white transition-colors duration-700">
+      <div className="w-full z-50 relative min-h-screen flex flex-col items-center justify-center gap-6 text-black dark:text-white transition-colors duration-700">
         <img src={alexanderLogo} className="w-72" alt="" />
 
         <h1 className="text-3xl md:text-6xl xl:text-8xl font-bold text-center">
@@ -95,7 +93,7 @@ function App() {
         </h1>
         <p className="-mt-4 opacity-70">Frontend Developer</p>
 
-        <div className="flex gap-4">
+        <div className="flex gap-4 relative z-[5]">
           <a
             href="https://wa.me/2349160979848?text=Hi%20Alexander%2C%20I’d%20like%20to%20hire%20you."
             target="_blank"
@@ -118,8 +116,8 @@ function App() {
           Password Modal
       ====================== */}
       {showPasswordUI && (
-        <div className="absolute inset-0 bg-white dark:bg-black flex flex-col items-center justify-center gap-10 z-20 transition-colors duration-700">
-          <p className="uppercase text-sm tracking-widest opacity-70">
+        <div className="absolute inset-0 dark:text-white bg-white dark:bg-black flex flex-col items-center justify-center gap-10 z-50 transition-colors duration-700">
+          <p className="uppercase text-sm tracking-widest opacity-70 ">
             Enter Passcode
           </p>
 
@@ -129,7 +127,7 @@ function App() {
               <div
                 key={i}
                 className={`w-5 h-5 rounded-full border transition
-                ${pin.length > i ? "bg-black dark:bg-white border-black dark:border-white" : "border-gray-400"}
+                ${pin.length > i ? "bg-black dark:bg-white border-black" : "border-gray-400"}
                 ${isError ? "animate-shake" : ""}`}
               />
             ))}
@@ -141,7 +139,10 @@ function App() {
               <button
                 key={i}
                 onClick={() => handleKeyPress(key)}
-                className="w-[5rem] h-[5rem] rounded-full text-2xl bg-black/5 dark:bg-white/10 active:scale-95 transition"
+                style={{
+                  animationDelay: isError ? `${i * 0.05}s` : `${i * 0.01}s`,
+                }}
+                className={`w-[5rem] h-[5rem] rounded-full ${isError ? "animate-shake" : ""} text-2xl font-medium bg-black/5 active:rounded-xl active:scale-95 dark:text-white dark:bg-white/10 transition duration-200 active:bg-black/80 active:text-white`}
               >
                 {key}
               </button>
@@ -150,19 +151,52 @@ function App() {
             {pin.length > 0 && (
               <button
                 onClick={clearKey}
-                className="absolute left-4 bottom-2 text-sm opacity-70"
+                aria-label="clear button"
+                className={`animate-fade absolute left-4 bottom-2 p-4 rounded-full duration-100 `}
               >
-                Clear
+                {" "}
+                <svg
+                  className="size-8"
+                  viewBox="0 0 16 16"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                >
+                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    {" "}
+                    <path
+                      d="m 7 2 c -0.832031 0 -1.558594 0.34375 -2.292969 0.78125 s -1.464843 1.003906 -2.128906 1.597656 c -0.660156 0.597656 -1.253906 1.222656 -1.707031 1.796875 c -0.226563 0.289063 -0.417969 0.5625 -0.570313 0.835938 c -0.152343 0.277343 -0.300781 0.53125 -0.300781 0.988281 s 0.148438 0.710938 0.300781 0.984375 c 0.152344 0.277344 0.34375 0.550781 0.570313 0.835937 c 0.453125 0.578126 1.046875 1.203126 1.707031 1.796876 c 0.664063 0.597656 1.394531 1.164062 2.128906 1.601562 s 1.460938 0.78125 2.292969 0.78125 h 6 c 1.644531 0 3 -1.355469 3 -3 v -6 c 0 -1.644531 -1.355469 -3 -3 -3 z m 1 3 c 0.265625 0 0.519531 0.105469 0.707031 0.292969 l 1.292969 1.292969 l 1.292969 -1.292969 c 0.1875 -0.1875 0.441406 -0.292969 0.707031 -0.292969 s 0.519531 0.105469 0.707031 0.292969 c 0.390625 0.390625 0.390625 1.023437 0 1.414062 l -1.292969 1.292969 l 1.292969 1.292969 c 0.390625 0.390625 0.390625 1.023437 0 1.414062 s -1.023437 0.390625 -1.414062 0 l -1.292969 -1.292969 l -1.292969 1.292969 c -0.390625 0.390625 -1.023437 0.390625 -1.414062 0 s -0.390625 -1.023437 0 -1.414062 l 1.292969 -1.292969 l -1.292969 -1.292969 c -0.390625 -0.390625 -0.390625 -1.023437 0 -1.414062 c 0.1875 -0.1875 0.441406 -0.292969 0.707031 -0.292969 z m 0 0"
+                      fill="currentColor"
+                    ></path>{" "}
+                  </g>
+                </svg>{" "}
               </button>
             )}
           </div>
 
           <button
             onClick={() => setShowPasswordUI(false)}
-            className="text-sm opacity-50 hover:opacity-100 transition"
+            className="text-sm opacity-50 hover:opacity-100 transition dark:text-white"
           >
             Cancel
           </button>
+
+          <a
+            style={{ animationDelay: isError ? `0.05s` : "0s" }}
+            href="https://wa.me/2349160979848?text=I'm%20trying%20to%20access%20your%20projects%20but%20I%20don't%20know%20the%20passcode"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="keypad"
+            className={`p-3 px-6 dark:bg-white/20 rounded-full active:rounded-xl text-xl font-medium text-white bg-black active:scale-95 transition duration-200`}
+          >
+            {" "}
+            Emergency{" "}
+          </a>
         </div>
       )}
     </div>
